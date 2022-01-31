ad3dd2f8c1534a74c39214511f401a0e8912136e
Sniperat <muhammadalixonakbarov2000@gmil.com>
2022-01-31 18:39:25 +0500

    admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import AbstractUser
 
+admin.site.registration(AbstractUser)
 # Register your models here.
--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ProductModel, CategoryModel
+
+admin.site.registration(ProductModel)
+admin.site.registration(CategoryModel)
 
 # Register your models here.
